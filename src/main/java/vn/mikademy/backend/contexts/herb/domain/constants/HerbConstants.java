2bb48df27169f9a049099107c6a12e3eff163142
Gideon <tranminhquan.dtp1b@gmail.com>
2026-01-19 20:36:17 +0700

    feat(constants): add HerbConstants for defining maximum lengths of herb attributes

---
+++ src/main/java/vn/mikademy/backend/contexts/herb/domain/constants/HerbConstants.java
@@ -0,0 +1,12 @@
+package vn.mikademy.backend.contexts.herb.domain.constants;
+
+
+
+public final class HerbConstants {
+
+    private HerbConstants() {}
+
+    public static final int MAX_NAME_LENGTH = 255;
+    public static final int MAX_SCIENTIFIC_NAME_LENGTH = 255;
+    public static final int MAX_DESCRIPTION_LENGTH = 1000;
+}
